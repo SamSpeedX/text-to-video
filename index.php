c9fe3ca061c03d0cd9606cb6c1efbec443fca331
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-17 08:53:33 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<html>
+  <head>
+    <title>Video generator</title>
+  </head>
+  <body>
+    <from>
+      <label for="text">You video idea</label> <br>
+      <input type="text" id="text">
+    </from>
+    <div class="result" id="result"></div>
+    <script src="assets/application/sam.js"></script>
+  </body>
+</html>
