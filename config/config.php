f6753e6ba66d12f47792b70106b7e34066a7a653
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-18 10:09:41 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,9 @@
 <?php
-// Adjust the path to autoload.php
 require __DIR__ . '/../vendor/autoload.php';
 
-// Adjust the path to the .env file
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+function env($key, $default = null) {
+    return $_ENV[$key] ?? $default;
+}
